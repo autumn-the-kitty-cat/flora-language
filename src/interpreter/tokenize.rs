9ee8f825d08683f201d01f70518fef75361c5bcc
autumn-the-kitty-cat <theodorebaker08@gmail.com>
2025-02-13 21:16:51 -0500

    tokenize

--- src/interpreter/tokenize.rs
+++ src/interpreter/tokenize.rs
@@ -1,5 +1,6 @@
 use std::{fs, path::Path};
 
+#[derive(Debug)]
 enum SimpleTokens {
     Operator(String),
     Identifier(String),
@@ -9,9 +10,56 @@ enum SimpleTokens {
 pub fn tokenize_file(file_path_string: String) {
     let mut file_path = Path::new(&file_path_string);
     let contents = fs::read_to_string(file_path).unwrap();
-    let lines = contents
+    let characters = contents
         .lines()
         .flat_map(|line| (line.to_string() + "\n").chars().collect::<Vec<char>>())
         .collect::<Vec<char>>();
-    println!("{:#?}",lines);
+
+    let mut token_start_index = 0;
+    let mut token_end_index = 0;
+    let mut char_index = 0;
+    let mut tokens = vec![];
+    while token_end_index < characters.len() {
+        if characters[token_end_index] == '/' && characters[token_end_index + 1] == '/' {
+            while characters[token_end_index] != '\n' {
+                token_end_index += 1;
+            }
+            token_start_index = token_end_index;
+        }
+        if characters[token_end_index].is_alphabetic() {
+            while characters[token_end_index].is_alphanumeric() {
+                token_end_index += 1;
+            }
+            let token = characters[token_start_index..token_end_index]
+                .iter()
+                .collect::<String>();
+            tokens.push(SimpleTokens::Identifier(token));
+            token_start_index = token_end_index;
+        } else if characters[token_end_index].is_numeric() {
+            while characters[token_end_index].is_numeric() || characters[token_end_index] == '.' {
+                token_end_index += 1;
+            }
+
+            let token = characters[token_start_index..token_end_index]
+                .iter()
+                .collect::<String>();
+            tokens.push(SimpleTokens::Number(token));
+            token_start_index = token_end_index;
+        } else if characters[token_end_index].is_ascii_punctuation() {
+            while characters[token_end_index].is_ascii_punctuation() {
+                token_end_index += 1;
+            }
+
+            let token = characters[token_start_index..token_end_index]
+                .iter()
+                .collect::<String>();
+            tokens.push(SimpleTokens::Operator(token));
+            token_start_index = token_end_index;
+        } else {
+            token_start_index += 1;
+            token_end_index += 1;
+        }
+    }
+
+    println!("{:#?}", tokens);
 }
